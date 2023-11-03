c0a90bdf19528fa8b553b552059575d725b737fc
Nick Strauch <61529274+NickStrauch13@users.noreply.github.com>
2023-11-03 20:45:48 +0000

    inital commit 2

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-def func(x):
-    return x + 1
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, expr
+
+
+spark = SparkSession.builder.appName("Week10MiniProject").getOrCreate()
+df = spark.read.csv("data.csv", header=True, inferSchema=True)
+
+
+# Apply a transformation (filtering)
+filtered_df = df.filter(col("some_column") > 10)
+
+# Perform a Spark SQL query
+df.createOrReplaceTempView("temp_table")
+result_df = spark.sql("SELECT AVG(some_column) FROM temp_table")
+
